2f71107a4eb0812cac8e1598eb8f10587d44db17
Muhamad Ikhwan <wic190025@siswa.um.edu.my>
2021-03-28 14:37:41 +0800

    update kmp

--- KMP_Algorithm.py
+++ KMP_Algorithm.py
@@ -1,16 +1,16 @@
 def kmp_matcher(string,pattern):
     n=len(string)
     m=len(pattern)
-    prefixArray=compute_prefix(pattern)
+    lps=compute_prefix(pattern)
     j=0
     for i in range(n):
         while j>0 and string[i]!=pattern[j]:
-            j=prefixArray[j-1]
+            j=lps[j-1]
         if string[i]==pattern[j]:
             j+=1
         if j==m:
             print("Pattern found in index ", (i-(j-1)))
-            j=prefixArray[j-1]
+            j=lps[j-1]
 
 
 def compute_prefix(pattern):
@@ -18,14 +18,12 @@ def compute_prefix(pattern):
     lps=[0]*m
     k=0
     for i in range(1,m):
+        while k>0 and pattern[k]!=pattern[i]:
+            k=lps[k-1]
         if pattern[k]==pattern[i]:
             k+=1
-            lps[i]=k
-        else:
-            k=0
-            lps[i]=k
+        lps[i]=k
     return lps
-
 
 string="algorithmisfun"
 pattern="fun"
