ebbcc2820b9530f7e9550fbe766c3d64fd0a25fe
Muhamad Ikhwan <wic190025@siswa.um.edu.my>
2021-03-29 22:50:44 +0800

    update kmp

--- KMP_Algorithm.py
+++ KMP_Algorithm.py
@@ -2,24 +2,25 @@ def kmp_matcher(string,pattern):
     n=len(string)
     m=len(pattern)
     lps=compute_prefix(pattern)
-    j=0
-    for i in range(n):
-        while j>0 and string[i]!=pattern[j]:
-            j=lps[j-1]
+    print(lps)
+    j=0                                                     # number of characters matched
+    for i in range(n):                                      # scan the text from left to right
+        while j>0 and string[i]!=pattern[j]:                
+            j=lps[j-1]                                      # next character does not match
         if string[i]==pattern[j]:
-            j+=1
-        if j==m:
-            print("Pattern found in index ", (i-(j-1)))
-            j=lps[j-1]
+            j+=1                                            # next character matches
+        if j==m:                                            # is all of pattern matched?
+            print("Pattern found in index ", (i-(j-1)))    
+            j=lps[j-1]                                      # look for the next match
 
 
 def compute_prefix(pattern):
     m=len(pattern)
     lps=[0]*m
-    k=0
+    k=0                                                     # 
     for i in range(1,m):
         while k>0 and pattern[k]!=pattern[i]:
-            k=lps[k-1]
+            k=lps[k-1]                                      # 
         if pattern[k]==pattern[i]:
             k+=1
         lps[i]=k
