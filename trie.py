d7bb7fc58710ced20d7b1f6933e9b4edeac5d1cc
afarhana00 <62510546+afarhana00@users.noreply.github.com>
2021-03-31 16:11:05 +0800

    update trie

--- trie.py
+++ trie.py
@@ -1,20 +1,20 @@
 def insert(word):
-  for i in range(len(word)):
+  for i in range(len(word)):      #loop for the step again after slice
       node = root
-      for letter in word:
+      for letter in word:         #loop for every letter
         if letter not in node:
-          node[letter] = {}
+          node[letter] = {}       #create new node
         node = node[letter]
-        node[letter] = True
-      word=word[1:]
+        node[letter] = True       #set the node for every letter to true
+      word=word[1:]               #slice the front letter by 1
 
 def search(find):
-  node = root
-  for letter in find:
+  node = root 
+  for letter in find:             #loop for every letter
     if letter not in node:
-      return False
+      return False                #check if the letter is not in trie
     node = node[letter]
-  return True
+  return True                     #the word is in trie
 
 root = {}
 word = "algorithmisfun"
